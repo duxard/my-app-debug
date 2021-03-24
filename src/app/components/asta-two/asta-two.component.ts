628d2cd4b393c6d77d088bc9050f83ab9dbe0ccb
duxard <duxard@gmail.com>
2021-03-24 21:57:39 +0300

    some commit #1

--- src/app/components/asta-two/asta-two.component.ts
+++ src/app/components/asta-two/asta-two.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-asta-two',
-  template: `<p>{{number}}</p>`
+  template: `<p> {{number}} </p>`
 })
 export class AstaTwoComponent {
 
